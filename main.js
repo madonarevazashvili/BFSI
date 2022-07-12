4dcef6f94177e745f0152bf74a0fb43ba21776d0
xruxunuka9077 <107389310+xruxunuka9077@users.noreply.github.com>
2022-07-12 13:28:56 +0400

    header

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-// let data;
-// await fetch('https://fakestoreapi.com/products/').then((response)=>{ return response.json()}).then((json)=> { data = json})
-// // axios.get('https://fakestoreapi.com/products/').then((response)=>{data=response.data})
-// console.log(data)
-// let row=document.querySelector('.row')
-// data.map((item)=>{
-//     let column = document.createElement('div')
-//     column.classList.add('col-3','mx-auto','my-2', 'bg-success')
-//     row.appendChild(column)
-//     let img = document.createElement('img')
-//     img.src = item.image
-//     column.appendChild(img)
-//     let name = document.createElement('p')
-//     name.innerHTML = item.title
-//     column.appendChild(name)
+document.querySelector("header_Right")
+let search = document.querySelector(".search")
+let img=document.querySelector(".img")
 
-// })
-// let data;
-// await fetch('https://covid-api.mmediagroup.fr/v1/cases').then((response) => { return response.json() }).then((json) => { data = json })
+img.addEventListener('click',function(){
+    search.classList.toggle('toggle')
+})
 
-// console.log(Object.entries(data))
 
 
