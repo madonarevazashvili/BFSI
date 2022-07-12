df0deebe7946c5773a6827107f86b694a7c61e22
xruxunuka9077 <107389310+xruxunuka9077@users.noreply.github.com>
2022-07-12 10:29:43 +0400

    start

---
+++ main.js
@@ -0,0 +1,23 @@
+// let data;
+// await fetch('https://fakestoreapi.com/products/').then((response)=>{ return response.json()}).then((json)=> { data = json})
+// // axios.get('https://fakestoreapi.com/products/').then((response)=>{data=response.data})
+// console.log(data)
+// let row=document.querySelector('.row')
+// data.map((item)=>{
+//     let column = document.createElement('div')
+//     column.classList.add('col-3','mx-auto','my-2', 'bg-success')
+//     row.appendChild(column)
+//     let img = document.createElement('img')
+//     img.src = item.image
+//     column.appendChild(img)
+//     let name = document.createElement('p')
+//     name.innerHTML = item.title
+//     column.appendChild(name)
+
+// })
+// let data;
+// await fetch('https://covid-api.mmediagroup.fr/v1/cases').then((response) => { return response.json() }).then((json) => { data = json })
+
+// console.log(Object.entries(data))
+
+
